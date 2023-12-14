e90e28af1c174d5ef04e94ac81c92175eed789d5
aidenwong812 <wongaiden812@gmail.com>
2023-12-15 00:53:28 +0900

    fix assistant page prop error
    Co-authored-by: aidenwong812 <wongaiden812@gmail.com>
    Co-authored-by: jacob143590 <jacob143590@gmail.com>

--- src/features/assistants/index.tsx
+++ src/features/assistants/index.tsx
@@ -1,68 +1,3 @@
-// import { useEffect, useState } from "react"
-// import { useDispatch, useSelector } from "react-redux"
-// import SearchBar from "../../components/Input/SearchBar"
-// import AssistantCard from "./components/AssistantCard"
-// import { PlusSmallIcon } from "@heroicons/react/24/outline"
-// import { AppDispatch, RootState } from "../../app/store"
-// import { openModal } from "../common/modalSlice"
-// import { MODAL_BODY_TYPES } from "../../utils/globalConstantUtil"
-// import { getAssistantContent } from "./assistantsSlice"
-
-// function Assistants() {
-
-//   const { assistants, isLoading } = useSelector((state: RootState) => state.assistant)
-//   const dispatch: AppDispatch = useDispatch()
-
-//   const [assistant, setAssistant] = useState(assistants)
-//   const [searchText, setSearchText] = useState("")
-
-//   useEffect(() => {
-//     dispatch(getAssistantContent());
-//   }, [])
-
-//   useEffect(() => {
-//     setAssistant(assistants)
-//   }, [assistants])
-
-//   const openAddNewAssistantModal = () => {
-//     dispatch(openModal({ title: "Add New Assistant", bodyType: MODAL_BODY_TYPES.ASSISTANT_ADD_NEW }))
-//   }
-
-//   // Search according to name
-//   // const applySearch = (value: string) => {
-//   //   let filteredAssistant = assistants.filter((t) => { return t.name.toLowerCase().includes(value.toLowerCase()) })
-//   //   setAssistant(filteredAssistant)
-//   // }
-
-//   if (isLoading) {
-//     return (
-//       <div className="flex justify-center items-center">
-//         <span className="loading loading-infinity w-32 h-32"></span>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <>
-//       <div className="flex items-center justify-between">
-//         <SearchBar searchText={searchText} styleClass="ml-4" setSearchText={setSearchText} />
-//         <button tabIndex={0} className="btn px-3 btn-sm normal-case btn-primary text-white sm:px-6" onClick={() => openAddNewAssistantModal()}>
-//           <PlusSmallIcon className="w-6 h-6 sm:hidden" />
-//           <span className="hidden sm:block">Add New</span>
-//         </button>
-//       </div>
-//       <div className="grid mt-4 grid-cols-1 md:grid-cols-3 xl:grid-cols-4 gap-6">
-//         {
-//           assistant.map((a, k) => (
-//             <AssistantCard key={k} assistant={a} />
-//           ))
-//         }
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Assistants
 // import moment from "moment"
 import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
@@ -80,15 +15,10 @@ import { Assistant } from "../../utils/Type"
 import { getAssistantContent } from "./assistantsSlice"
 
 type TopSidePropTypes = {
-  applySearch: Function,
-  selected: string
+  applySearch: Function
 }
 
-type PropTypes = {
-  assistant_id: string
-}
-
-const TopSideButtons = ({ applySearch, selected }: TopSidePropTypes) => {
+const TopSideButtons = ({ applySearch }: TopSidePropTypes) => {
 
   const dispatch = useDispatch()
 
@@ -115,7 +45,6 @@ const TopSideButtons = ({ applySearch, selected }: TopSidePropTypes) => {
       <SearchBar searchText={searchText} styleClass="mr-4" setSearchText={setSearchText} />
       <button
         className="btn px-3 btn-sm normal-case btn-primary text-white sm:px-6"
-        disabled={selected === '-1'}
         onClick={() => openAddNewAssistantModal()}
       >
         <PlusSmallIcon className="w-6 h-6 sm:hidden" />
@@ -125,7 +54,7 @@ const TopSideButtons = ({ applySearch, selected }: TopSidePropTypes) => {
   )
 }
 
-function Assistants({ assistant_id }: PropTypes) {
+function Assistants() {
 
   const { assistants, isLoading } = useSelector((state: RootState) => state.assistant)
   const dispatch: AppDispatch = useDispatch()
@@ -166,11 +95,16 @@ function Assistants({ assistant_id }: PropTypes) {
   }
 
   return (
-    <TitleCard title="Assistants" topMargin="mt-2" TopSideButtons={<TopSideButtons selected={assistant_id} applySearch={applySearch} />}>
+    <TitleCard title="Assistants" topMargin="mt-2" TopSideButtons={<TopSideButtons applySearch={applySearch} />}>
 
       {/* Team Member list in table format loaded constant */}
       {
-        assistant.length !== 1 || assistant[0].assistant_name !== "" ? (
+        assistant.length === 0 || assistant.length === 1 && assistant[0].assistant_name === "" ? (
+          <div className="text-2xl flex justify-center items-center gap-2 text-center">
+            <FaceFrownIcon className="w-12 h-12" />
+            No Assistants
+          </div>
+        ) : (
           <div className="overflow-x-auto w-full">
             <table className="table w-full table-sm lg:table-lg">
               <thead>
@@ -202,11 +136,6 @@ function Assistants({ assistant_id }: PropTypes) {
                 }
               </tbody>
             </table>
-          </div>
-        ) : (
-          <div className="text-2xl flex justify-center items-center gap-2 text-center">
-            <FaceFrownIcon className="w-12 h-12" />
-            No Assistants
           </div>
         )
       }
